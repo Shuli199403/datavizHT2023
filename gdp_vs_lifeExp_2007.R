ae2e8356b02bf5ab564ddded0880372f0c343f71
Shu Li <144832463+Shuli199403@users.noreply.github.com>
2023-10-04 16:41:20 +0200

    Comment made by Shu Li
    make a comment

--- gdp_vs_lifeExp_2007.R
+++ gdp_vs_lifeExp_2007.R
@@ -5,9 +5,11 @@ gapminder_2007 <- subset(gapminder, year == 2007)
 
 ggplot(gapminder_2007, aes(x=log(gdpPercap), y=lifeExp, color=continent)) +
   geom_point(aes(size=pop), alpha=0.7) +
-  labs(title="GDP per capita vs. Life Expectancy (2007)",
+  labs(title="GDP per capita vs. Life expectancy (2007)",
        x="Log of GDP per capita",
        y="Life expectancy",
        subtitle="Data from Gapminder",
        caption="Colored by continent") +
   theme_minimal()
+
+#comment made by Shu Li
